3d6959f9519c0fa5315a81cd16facf815d1bf20d
nvdatj2002 <92840303+nvdatj2002@users.noreply.github.com>
2022-07-14 16:54:26 +0700

    git clone

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactsssssss
         </a>
       </header>
     </div>
